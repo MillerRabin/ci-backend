3287764ab09998e6ae7ecf09c8d6c8c3011d0778
MillerRabin <millerrabin@raintech.su>
2018-04-08 23:46:36 +0300

    create tables

--- sql/tableStruct.sql
+++ sql/tableStruct.sql
@@ -3,3 +3,25 @@ create user master;
 grant all on database ci to master;
 alter user master password 'ifyouwanttohave';
 alter user postgres password 'ifyouwanttohave';
+
+create table git_logs (
+    event_time timestamp with time zone not null default now(),
+    event_data jsonb
+);
+
+create table projects (
+    id serial primary key,
+    reg_time timestamp with time zone not null default now(),
+    update_time timestamp with time zone not null default now(),
+    project_name varchar(100),
+    branch varchar(100),
+    init jsonb,
+    deploy jsonb,
+    credentials jsonb
+);
+
+insert into projects (project_name, branch, init, deploy, credentials)
+values ('ci-backend', 'production', '[ "cd /usr/raintech/ci", "git clone git@bitbucket.org:raintechteam/ci-backend.git" ]',
+'["cd /usr/raintech/ci/ci-backend", "git pull origin production"]',
+'{ "host": "ci.raintech.su", "user": "ci", "password": "ifyouwanttohave"}'
+)
