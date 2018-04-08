feab4d5851e44ff97ae8c3a814de3f1489420c92
MillerRabin <millerrabin@raintech.su>
2018-04-08 23:51:12 +0300

    add log support

--- modules/git/git.js
+++ modules/git/git.js
@@ -3,13 +3,24 @@ const Router = require('koa-router');
 const koaBody = require('koa-body');
 const response = require('../../middlewares/response.js');
 
+async function logGit(connection, data) {
+    const addQuery = 'insert into git_logs (event_data) values($1)';
+    const params = [data];
+    return await connection.query(addQuery, params);
+}
+
 exports.addController = (application, controllerName) => {
     const router = new Router();
 
     router.post('/' + controllerName + '/bitbucket', koaBody(), async (ctx) => {
         const data = ctx.request.body;
-        console.log(data);
-        return { success: 'true'};
+        const connection = await application.pool.connect();
+        try {
+            await logGit(connection, data);
+            return { success: true };
+        } finally {
+            await connection.release();
+        }
     });
 
     application.use(router.routes());
