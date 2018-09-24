5dbf57d2fd7f56933b233c069586d35a41d9e68a
MillerRabin <millerrabin@raintech.su>
2018-09-24 14:45:32 +0300

    log gitlab data

--- modules/git/git.js
+++ modules/git/git.js
@@ -72,6 +72,7 @@ exports.addController = (application, controllerName) => {
         const owner = (dr.project == null) ? null : dr.project.owner;
         logData.project = dr.project;
         setTimeout(async () => {
+            console.log(deployResult);
             if (Array.isArray(deployResult)) {
                 for (let dr of deployResult)
                     if (dr.reload != null) dr.reload = await dr.reload();
